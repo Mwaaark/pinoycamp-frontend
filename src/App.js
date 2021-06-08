8025140705e7ab46de9f379c9190bbd91b84b6ab
Mark Robert Bumatay <kramtrebor1995@gmail.com>
2021-06-08 13:28:14 +0800

    add protected routes if not loggedIn

--- src/App.js
+++ src/App.js
@@ -1,64 +1,60 @@
-import React from "react";
-import Layout from "./components/layout/Layout";
+import React, { useContext, Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router";
-import Index from "./pages/Campgrounds/Index";
-import NotFound from "./pages/NotFound";
-import New from "./pages/Campgrounds/New";
-import Show from "./pages/Campgrounds/Show";
-import Edit from "./pages/Campgrounds/Edit";
-import Register from "./pages/Users/Register";
-// import AuthContext from "./context/auth-context";
-import Login from "./pages/Users/Login";
+import AuthContext from "./context/auth-context";
+import Layout from "./components/layout/Layout";
+import { Spinner } from "react-bootstrap";
+
+const Index = React.lazy(() => import("./pages/Campgrounds/Index"));
+const New = React.lazy(() => import("./pages/Campgrounds/New"));
+const Show = React.lazy(() => import("./pages/Campgrounds/Show"));
+const Edit = React.lazy(() => import("./pages/Campgrounds/Edit"));
+const Register = React.lazy(() => import("./pages/Users/Register"));
+const Login = React.lazy(() => import("./pages/Users/Login"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 export default function App() {
-  // const authCtx = useContext(AuthContext);
-
-  // let routes;
-
-  // if (authCtx.token) {
-  //   routes = (
-  //     <Switch>
-  //       <Route path="/" exact>
-  //         <Redirect to="/campgrounds" />
-  //       </Route>
-  //       <Route path="/campgrounds" component={Index} exact />
-  //       <Route path="/campgrounds/new" component={New} exact />
-  //       <Route path="/campgrounds/:id" component={Show} exact />
-  //       <Route path="/campgrounds/:id/edit" component={Edit} exact />
-  //       <Route path="*" component={NotFound} />
-  //     </Switch>
-  //   );
-  // } else {
-  //   routes = (
-  //     <Switch>
-  //       <Route path="/" exact>
-  //         <Redirect to="/campgrounds" />
-  //       </Route>
-  //       <Route path="/register" component={Register} exact />
-  //       <Route path="/login" component={Login} exact />
-  //       <Route path="/campgrounds" component={Index} exact />
-  //       <Route path="/campgrounds/:id" component={Show} exact />
-  //       <Route path="*" component={NotFound} />
-  //     </Switch>
-  //   );
-  // }
-
-  // TRY TO IMPLEMENT LIKE PER ROUTE, REDIRECT IF NOT LOGGED IN
+  const authCtx = useContext(AuthContext);
 
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/campgrounds" />
-        </Route>
-        <Route path="/register" component={Register} exact />
-        <Route path="/login" component={Login} exact />
-        <Route path="/campgrounds" component={Index} exact />
-        <Route path="/campgrounds/new" component={New} exact />
-        <Route path="/campgrounds/:id" component={Show} exact />
-        <Route path="/campgrounds/:id/edit" component={Edit} exact />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="text-center">
+            <Spinner animation="border" role="status">
+              <span className="sr-only"></span>
+            </Spinner>
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/campgrounds" />
+          </Route>
+          <Route path="/register" exact>
+            {authCtx.token ? <Redirect to="/campgrounds" /> : <Register />}
+          </Route>
+          <Route path="/login" exact>
+            {authCtx.token ? <Redirect to="/campgrounds" /> : <Login />}
+          </Route>
+          <Route path="/campgrounds" component={Index} exact />
+          <Route path="/campgrounds/new" exact>
+            {authCtx.token ? <New /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/campgrounds/:id" component={Show} exact />
+          <Route
+            path="/campgrounds/:id/edit"
+            exact
+            render={({ match }) =>
+              authCtx.token ? (
+                <Edit />
+              ) : (
+                <Redirect to={`/campgrounds/${match.params.id}`} />
+              )
+            }
+          />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
